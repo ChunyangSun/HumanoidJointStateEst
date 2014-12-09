8dcb8455f5b35ecd1df7635e1cd43f93e5fe08b2
Jacob Charles Walker <jcwalker@gs10262.sp.cs.cmu.edu>
2014-12-08 22:31:23 -0500

    Gaussian Processes are ready to go. Datasets are included.

--- DisneySky/GaussianProcess.m
+++ DisneySky/GaussianProcess.m
@@ -1,15 +1,50 @@
+function GaussianProcess(trainInput, trainMocap, testInput, testMocap, jointInd)
 
+[finalX, removalInds]  = parseMocap(trainMocap, jointInd)
 
+[finalXTest, removalIndsTest]  = parseMocap(testMocap, jointInd)
+
+x = trainInput/1950;
+
+x = x(:,2:40);
+
+x = x(removalInds,:);
+
+xtest = testInput/1950;
+
+xtest = xtest(:,2:40);
+
+predictedMeans = [];
+
+for ourVar = 1:3
+y = finalX(:,ourVar);
+  meanfunc = {@meanConst}; hyp.mean = [0];
+  covfunc = {@covSEiso}; ell = 1/4; sf = 1; hyp.cov = log([ell; sf]);
+  likfunc = @likGauss; sn = 0.1; hyp.lik = log(sn);
+ 
+  [m s2] = gp(hyp, @infExact, meanfunc, covfunc, likfunc, x, y, xtest);
+  predictedMeans = [predictedMeans m]
+end
+
+clear figure;
+
+plot3(predictedMeans(:,1), predictedMeans(:,2), predictedMeans(:,3));
+hold on;
+plot3(finalX(:,1), finalX(:,2), finalX(:,3), 'Color', 'red');
+plot3(finalXTest(:,1), finalXTest(:,2), finalXTest(:,3), 'Color', 'green');
+
+end
+
+function [finalX, removalInds]  = parseMocap(mocap, j)
 
 finalX = [];
-x = ourInput/1950;
-x = [x;x(800,:)];
 
-for j = 2:5
-Joint = withMocap1{j,1}
+removalInds = [];
+
+Joint = mocap{j,1}
 
 
-T = withMocap1(:,1);
+T = mocap(:,1);
 T = cell2mat(T);
 
 ourLines = find(T == Joint);
@@ -19,72 +54,29 @@ Y = []
 Z = []
 
 for i = 2:size(ourLines, 1)
-    X = [X; {withMocap1{ourLines(i),2}}];
-    Y = [Y; {withMocap1{ourLines(i),3}}];
-    Z = [Z; {withMocap1{ourLines(i),4}}];
+    
+    if(i > 2 && abs(X{end} - mocap{ourLines(i),2}) > 20)
+        continue;
+    end
+    
+    if(i > 2 && abs(Y{end} - mocap{ourLines(i),3}) > 20)
+
+        continue;
+        
+    end
+    
+    if(i > 2 && abs(Z{end} - mocap{ourLines(i),4}) > 20)
+        continue;
+    end
+    removalInds = [removalInds; i - 1];
+    
+    X = [X; {mocap{ourLines(i),2}}];
+    Y = [Y; {mocap{ourLines(i),3}}];
+    Z = [Z; {mocap{ourLines(i),4}}];
 end
+
 
 finalX = [finalX cell2mat(X) cell2mat(Y) cell2mat(Z)];
 
 end
-
-x(finalX(:,1) < 0,:) = []
-finalX(finalX(:,1) < 0,:) = []
-
-x(finalX(:,2) < 0,:) = []
-finalX(finalX(:,2) < 0,:) = []
-
-x(finalX(:,3) < 0,:) = []
-finalX(finalX(:,3) < 0,:) = []
-
-x = x(:,2:40);
-
-%theMean = mean(x);
-%x = x - repmat(theMean, size(x,1), 1);
-
-%theSigma = x' * x / size(x,2);
-
-%[U,S,V] = svd(theSigma);
-
-%x = x * U *diag(1./sqrt(diag(S) + 0.001));
-
-x = x(:,1:9);
-
-%P = pca(x);
-
-%P = P(:,1:7);
-
-%x = x*P;
-
-xtest = withMocap2/1950;
-xtest = withMocap3/1950;
-
-xtest = xtest(:,2:40);
-
-%xtest = xtest - repmat(theMean, size(xtest,1), 1);
-
-%theSigma = xtest' * xtest / size(xtest,2);
-
-%[U,S,V] = svd(theSigma);
-
-%xtest = xtest * U *diag(1./sqrt(diag(S) + 0.001));
-
-xtest = xtest(:,1:9);
-
-predictedMeans = [];
-
-for ourVar = 1:3
-y = finalX(:,ourVar);
-  meanfunc = {@meanConst}; hyp.mean = [mean(y)];
-  covfunc = {@covSEiso}; ell = 2.5; sf = 1; hyp.cov = log([ell; sf]);
-  likfunc = @likGauss; sn = 0.1; hyp.lik = log(sn);
- 
-  [m s2] = gp(hyp, @infExact, meanfunc, covfunc, likfunc, x, y, xtest);
-  predictedMeans = [predictedMeans m]
-end
-
-mean(norm(y - finalX(:, 1:3)))
-
-plot3(predictedMeans(:,1), predictedMeans(:,2), predictedMeans(:,3));
-plot3(finalX(:,1), finalX(:,2), finalX(:,3));
 
